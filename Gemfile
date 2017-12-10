bfab15a3e61b5e810a133691e2ddedf681af7162
dzucconi <mail@damonzucconi.com>
2017-12-10 16:22:26 -0500

    Configures webpack

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'autoprefixer-rails'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.7'
 gem 'rails', '~> 5.1.4'
---
+++ front/packs/application.js
@@ -0,0 +1,7 @@
+import "./application.css";
+
+const init = () => {
+  document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
+}
+
+document.addEventListener('DOMContentLoaded', init);
